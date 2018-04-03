b7655a79b73c6ee652284df0c0dbce084081da10
tsettset <tsettset56@gmail.com>
2018-04-03 21:53:52 +0200

    test envoie site front end vers git

---
+++ accueil.php
@@ -0,0 +1,250 @@
+<?php
+
+include'tete.php' 
+
+?>
+
+<br>
+
+
+<div class="container" style="padding : 0;">
+    <div class="row">
+        <div id="MagicCarousel" class="carousel slide col-md-8" data-ride="carousel" data-interval="false">
+            <ol class="carousel-indicators"> 
+                <li data-target="#MagicCarousel" data-slide-to="0"></li>
+                <li data-target="#MagicCarousel" data-slide-to="1"></li>
+                <li data-target="#MagicCarousel" data-slide-to="2"></li>
+                <li data-target="#MagicCarousel" data-slide-to="3"></li>
+                <li data-target="#MagicCarousel" data-slide-to="4"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item">
+
+
+                    <img class="d-block w-100" src="img/img1.jpg" alt="first slide">
+                    <div class="carousel-caption" style="bottom :0;left :0; right:0;">
+                        &nbsp; &nbsp; &nbsp;(First Slide)
+                    </div>
+
+                </div>
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/img2.jpg" alt="second slide">
+                    <div class="carousel-caption" style="bottom :0;left :0; right:0;">
+                        &nbsp; &nbsp; &nbsp;(Second Slide)
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img3.jpg" alt="third slide">
+                    <div class="carousel-caption" style="bottom :0;left :0; right:0;">
+                        &nbsp; &nbsp; &nbsp;(Third Slide)
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img4.jpg" alt="fourth slide">
+                    <div class="carousel-caption" style="bottom :0;left :0; right:0;">
+                        &nbsp; &nbsp; &nbsp;(Fourth Slide)
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img5.jpg" alt="fifth slide">
+                    <div class="carousel-caption" style="bottom :0;left :0; right:0;">
+                        &nbsp; &nbsp; &nbsp;(Fifth Slide)
+                    </div>
+                </div>
+            </div>
+            <!--<a class="carousel-control-prev" href="#MagicCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#MagicCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+                <span class="sr-only">Next</span>
+            </a>--> 
+        </div>
+        <div class="col-md-4">
+            <figure class="figure">
+                <img src="img/img1.jpg" class="img-fluid" alt="carrelage">
+                <figcaption class="figure-caption text-white text-center">A caption for the above image.</figcaption>
+            </figure>
+
+            <figure class="figure">
+                <img src="img/img1.jpg" class="img-fluid" alt="carrelage" >
+                <figcaption class="figure-caption text-white text-center">A caption for the above image.</figcaption>
+            </figure>
+
+        </div>    
+
+    </div>
+
+
+
+
+    <div class="row">
+        <div class="col">
+            <p><img class="img-fluid" src="img/cadenas.png" alt="paiement securise"> Paiement sécurisé</p>
+            <p><img class="img-fluid" src="img/camion.png" alt="paiement securise"> Livraison à domicile</p>
+        </div>
+        <div class="col">
+            <p><img class="img-fluid" src="img/casse.png" alt="anti casse"> Garantie anti-casse</p>
+            <p><img class="img-fluid" src="img/echantillon.png" alt="paiement securise"> Echantillon gratuit</p>
+        </div>
+        <div class="col">
+            <p><img class="img-fluid" src="img/satisfait.png" alt="paiement securise"> Satisfait ou remboursé</p>
+            <p><img class="img-fluid" src="img/echange.png" alt="paiement securise"> Echange et retour facile</p>
+        </div> 
+        <div class="col">
+            <p><img class="img-fluid" src="img/euros.png" alt="paiement securise"> Facilité de paiement 3x sans frais dès xxx €</p>
+        </div>
+
+        <br>
+        <br>
+    </div>
+
+
+
+
+    <div class="jumbotron">
+
+        <div class="produits">NOS PRODUITS</div>
+        <div class="triangle"></div>
+        <hr>
+    </div>
+    <br>
+    <div class="jumbotron">
+        <div class="row">
+            <div class="col mr-6">
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+
+
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+            </div>
+
+            <div class="col ml-6">
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+
+                <div class="ajuster text-center">
+                    <img class="img-fluid" src="img/img4.jpg" alt="imag4"> 
+                    <div class="container-fluid">
+                        <div class="col">
+                            Carreaux de ciment imitation
+                        </div>
+                        <div class="col couleur">
+                            19,99 € TTC /m²
+                        </div>
+                        <div class="row">
+                            <div class="col bg-dark text-white"><img class="img-fluid" src="img/loupe2.png" alt="vue">&nbsp;&nbsp;Voir le produit</div>
+                            <div class="col fond text-white">Ajout au panier</div>
+                        </div>
+
+                    </div>
+                    <div class="col coulgris text-white">
+                        <img class="img-fluid" src="img/devis2.png" alt="devis">&nbsp;&nbsp;Demande de devis personnalisé
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <?php 
+
+    include'pied.php';
+
+    ?>
---
+++ accueil_infos.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Accueil bas</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">		
+		<link rel="stylesheet" type="text/css" href="accueil_infos.css">
+		
+	</head>
+	<body>
+
+		<div class="container" >
+			<fieldset class="accueil_infos">
+				<legend>
+					<div class="etiquette_accueil_bas">ESHOP</div>
+					<div class="arrow-right"></div>
+				</legend>
+				<div class="row hauteur-info-10"></div>
+				<div class="row">
+					<div class="col-md-4 rightBorderQuery">					
+						<div class="row">
+							<div class="col-md-4 accueil_icon_cell"><img src="img/location.jpg" alt="adress icon"></div>
+							<div class="col-md-8 accueil_infos_cell">
+								<p><strong>Adresse :</strong></p>
+								<p>Avenue Leclerc <br> 75015 Paris</p>
+							</div>						
+						</div>
+						<div class="row">
+							<div class="col-md-4 accueil_icon_cell"><img src="img/telephone.jpg" alt="phone icon"></div>
+							<div class="col-md-8 accueil_infos_cell">
+								<p><strong>Téléphone :</strong></p>
+								<p>Tel : 01 28 02 29 45 <br> Fax : 01 60 05 12 33</p>
+							</div>						
+						</div>
+						<div class="row">
+							<div class="col-md-4 accueil_icon_cell"><img src="img/letterbox.jpg" alt="email icon"></div>
+							<div class="col-md-8 accueil_infos_cell">
+								<p><strong>Email :</strong></p>
+								<p>contact@eshop.fr</p>
+							</div>						
+						</div>
+						
+					</div>
+					<div class="col-md-4 rightBorderQuery">
+						<div class="row">
+							<div class="col-md-4 accueil_icon_cell"><img src="img/clock.jpg" alt="opening hours icon"></div>
+							<div class="col-md-8 accueil_infos_cell">
+								<p><strong>Horaires d'ouverture :</strong></p>
+								<p>du lundi au vendredi de 8h à 12h et de 13h30 à 18h00</p>
+								<p>le samedi de 9h à 17h sans interruption</p>
+								<p>Service de livraison et enlèvement des marchandises jusqu'à 18h et le samedi jusqu'à 17h</p>
+							</div>						
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-md-4 accueil_icon_cell"><img src="img/people.jpg" alt="who are we icon"></div>
+							<div class="col-md-8 accueil_infos_cell">
+								<p><strong>Qui sommes-nous ?</strong></p>
+								<p>Spécialiste du carrelage et du parquet, ESHOP vous accompagne dans vos projets de construction ou de rénovation.</p>
+								<p>Entreprise familiale depuis 35 ans, nous possédons l'expérience et le savoir faire alliant méthodes traditionnelles et technique actuelle.</p>							
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row last_info_cell">
+					<div class="col-md-12">
+						Venez découvrir tous nos produits directement en boutique !
+					</div>
+				</div>
+			</fieldset>
+		</div>
+	<script src="jquery-3.3.1.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	</body>
+</html>
