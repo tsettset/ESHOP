9d8b043923643bf676cd038fd0a77c92b583c706
tsettset <tsettset56@gmail.com>
2018-04-11 14:46:45 +0200

    ajout des images carr et parq

---
+++ test.php
@@ -0,0 +1,66 @@
+<?php
+
+include 'tete.php';
+
+?>
+       
+
+       <body>
+        <div class="container">
+            <header class="row no-padding" style="height : 75px;">
+
+                <div class="col-md-9">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="#">
+                        <img class="img-fluid" src="portfolio-2903909_1280.png" title="mon portfolio" alt="un portfolio">
+                    </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">CV en Ligne</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Veille
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Frameworks</a>
+                                        <a class="dropdown-item" href="#">Nouveautés applications</a>
+                                        <a class="dropdown-item" href="#">Versions Langages de programmation</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                   </div>
+                <div class="col-md-2 offset-md-1 fond_orange">Header 3</div>
+ 
+
+            </header>
+           <br>
+
+            <section class="row jumbotron jumbotron-fluid border border-info rounded" style="height : 400px">
+                <div class="col-md-2 "></div>
+                <div class="col-md-6 offset-md-1 "></div>
+                <div class="col-md-2 offset-md-1"></div>
+            </section>
+            <footer class="row text-center" style="height : 150px">
+                <div class="col-md-2 fond_violet">footer 1</div>
+                <div class="col-md-6 offset-md-1 fond_violet">footer 2</div>
+                <div class="col-md-2 offset-md-1 fond_violet">footer 3</div>
+            </footer>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
