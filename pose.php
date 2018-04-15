80534551fe189a7dd5754e02d3b6b695bbd1e172
Hatem-D <hdourai@gmail.com>
2018-04-16 01:44:15 +0200

    pose fini

--- pose.php
+++ pose.php
@@ -4,7 +4,7 @@
   <?php
   include 'head.php';
   ?>
-
+  <link rel="stylesheet" href="css/pose.css">
 </head>
 <body>
   <?php include 'tete.php'; ?>
@@ -156,149 +156,45 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 border">
+              <form action="#" class="ws-validate">
+                <h5>Date de rendez-vous :</h5>
+                <div class="form-row">
+                  <input type="date" class="hide-replaced" />
+                </div>
+                <div class="form-group">
+                  <label for="heure">
+                    Selectionnez une plage horaire
+                  </label>
+                  <select id="heure" name="heure" class="form-control">
+                    <option value="matin" selected="">Matin (9h-12h)</option>
+                    <option value="aprem">Apres-midi (13h-17h)</option>
+                  </select>
+                </div>
+                <button type="submit" class="btn btn-info" id="btnLogin">
+                  <small><em>valider</em></small>
+                </button>                
+              </form>
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
 
   </div>
 
-
-  <?php
-  include 'accueil_infos.php';
-  ?>
 </div>
+
+
+<?php
+include 'accueil_infos.php';
+?>
+</div>
+<script src="js/pose.js"></script>
 <?php
 include 'js_inc.php';
+
 ?>
+
 </body>
 </html>
-
-
-
-
-
-
-<!--<div class="container">
-<div class="row">
-<div class="col-md-8">
-<div class="well well-sm">
-<div class="row">
-<div class="col-md-4 text-right"><h5>Vos informations</h5></div>
-<div class="col-md-4">
-<form action="#">
-<button type="submit" class="btn btn-info" id="btnLogin">
-<small><em>Connexion ou inscription</em></small>
-</button>
-</form>
-</div>
-</div>
-<div class="row">
-<form>
-<div class="col-md-6">
-<div class="form-group row">
-<div class="col-md-6">
-<label for="name">
-Nom
-</label>
-<input type="text" class="form-control" id="name" placeholder="Votre nom *" required="required" />
-</div>
-<div class="col-md-6">
-<label for="prenom">
-Prenom
-</label>
-<input type="text" class="form-control" id="name" placeholder="Votre prenom *" required="required" />
-</div>
-</div>
-<div class="form-group row">
-<div class="row">
-<div class="col-md-12">Votre adresse</div>
-</div>
-<div class="row">
-<div class="col-md-3">
-<label for="complement1">
-<small>Numero</small>
-</label>
-<input type="text" class="form-control" id="complement1" placeholder="123..." />
-</div>
-<div class="col-md-8  ">
-<label for="complement2">
-<small>rue, lieu-dit</small>
-</label>
-<input type="text" class="form-control" id="complement2" placeholder="Rue, lieu-dit" />
-</div>
-</div>
-<div class="row">
-<div class="col-md-3">
-<label for="code_postal">
-<small>Code Postal</small>
-</label>
-<input type="text" class="form-control" id="code_postal" placeholder="123..." />
-</div>
-<div class="col-md-8  ">
-<label for="ville">
-<small>Ville, commune</small>
-</label>
-<input type="text" class="form-control" id="ville" placeholder="Rue, lieu-dit" />
-</div>
-</div>
-</div>
-</div>
-<div class="col-md-6">
-<div class="form-group">
-<label for="subject">
-Type de pose
-</label>
-<select id="subject" name="subject" class="form-control" required="required">
-<option value="na" selected="">Choisissez:</option>
-<option value="carrelage">Carrelage</option>
-<option value="exterieur">Exterieur</option>
-<option value="parquet">Parquet</option>
-</select>
-</div>
-<div class="form-group">
-<label for="name">
-Remarque
-</label>
-<textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
-placeholder="Message"></textarea>
-<button type="submit" class="btn btn-info" id="btnContactUs">
-Envoyer
-</button>
-</div>
-</div>
-</form>
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<form>
-<legend><span class="glyphicon glyphicon-globe"></span> Nos locaux:</legend>
-<address>
-<strong>Eshop</strong><br>
-Avenue Leclerc<br>
-75015 Paris<br>
-<abbr title="Phone">
-Tel:
-</abbr>
-01 28 02 29 45
-<abbr title="Phone">
-Fax:
-</abbr>
-01 60 05 12 33
-</address>
-<address>
-<strong>Adresse de contact</strong><br>
-<a href="mailto:contact@eshop.fr">contact@eshop.fr</a>
-</address>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
--->
